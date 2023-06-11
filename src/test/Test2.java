a86f11b18e7a861533ad4fd7105c648df679450c
Serdar-Gul <ramserkan135@gmail.com>
2023-06-11 15:43:29 +0300

    first commit, general update

---
+++ src/test/Test2.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test2 {
+    public static void main(String[] args) {
+        //test2
+        System.out.println("Test1");
+        System.out.println("Test2");
+    }
+}
