8864371f38e15c3795e0060d2ffb5e7893d183b8
Serdar-Gul <ramserkan135@gmail.com>
2023-06-11 16:11:27 +0300

    added: Test2.java

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -3,7 +3,7 @@ package test;
 public class Test2 {
     public static void main(String[] args) {
         //test2
-        System.out.println("Test1");
+        System.out.println("invalid test");
         System.out.println("Test2");
     }
 }
